b75163afc4a84bfaf1e186621a57a03c3c2c90cf
Guntars Simanskis <guntars@diamondgraphics.ie>
2018-08-27 14:13:31 +0100

    Added Notes addon

--- src/widgets/btnAction/BtnAction.js
+++ src/widgets/btnAction/BtnAction.js
@@ -3,12 +3,12 @@ import template from './_btnAction';
 import {set, view, lensPath} from '../../client/utils/lenses';
 import {compose} from '../../client/utils/curry';
 
-const respLens    = view(lensPath('resp'));
+// const respLens    = view(lensPath('resp'));
 const cssLens     = set(lensPath('css'));
 const scriptsLens = set(lensPath('scripts'));
 
-const widget = (btnLens) => task((_ = {}) => compose(
-    set(btnLens, template(respLens(_))),
+const widget = (btnLens, btnMessage) => task((_ = {}) => compose(
+    set(btnLens, template(view(btnMessage)(_))),
     cssLens(['/widgets/btnAction/style.css']),
     scriptsLens(['/main.js', '/widgets/btnAction/main.js',])
 )(_));
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,18 +1,25 @@
-import btnAction from '../src/widgets/btnAction/BtnAction'
+import btnAction from '../src/widgets/btnAction/BtnAction';
+import {task} from 'functional/core/Task';
 
 import {storiesOf} from '@storybook/html';
 import {action} from '@storybook/addon-actions';
+import { withNotes } from '@storybook/addon-notes';
 
-import {lensPath} from '../src/client/utils/lenses';
+
+import {lensPath, set} from '../src/client/utils/lenses';
 
 import storyAdder from './storyAdder';
 
 const widgetLens = lensPath('content');
+const btnTextLens = lensPath('btnText');
+
+const fancyButton = task(_ => set(btnTextLens, 'This Is Fancy Button')(_)).through(btnAction(widgetLens, btnTextLens));
 
 const testPattern = ({evtBus}) => {
     evtBus.subscribe('mdw-btnAction-trigger', _ => action('btnClick')(_));
 };
 
+
 storiesOf('Demo', module)
     .add('heading', () => '<h1>Hello World</h1>')
-    .add('button', () => storyAdder(btnAction(widgetLens))(_=>testPattern(_)));
+    .add('button', withNotes('A very simple component')(() => storyAdder(fancyButton)(_ => testPattern(_))));
